abd18c8a672822c71cc3b477cb88df5ebb792127
popoiopo <bastiaan.chatel@gmail.com>
2016-04-06 22:01:13 +0200

    werkend!
    Kan nu zien wie er waar zit er hoeveel er waar moeten

--- roostering.py
+++ roostering.py
@@ -1,16 +1,37 @@
+"""
+ola! Dit is even een python scriptje om alle data
+van alle studenten erin te krijgen.
+
+Zo kunnen we van elke student weten in welke klassen die zit,
+hoeveel mensen er in elke klas zitten etc.
+"""
+
 import csv
+from fnmatch import fnmatch, fnmatchcase
 
 rooster = open("studenten_roostering.csv")
 csv_rooster = csv.reader(rooster)
 
-data = []
-for row in csv_rooster:
-	data.append(row)
+data = [row for row in csv_rooster]
+var_names = [name for name in data[0]]
+people_var = [dict(zip(var_names, person)) for person in data]
+people_person = [dict(zip(person, var_names)) for person in data]
 
-d = {data[0][0]:data[1][0]}
-print d
+for person in people:
+	print person['Voornaam']
+	print ""
 
+for person in people:
+	if person['Vak1'] in 'Webprogrammeren en databases':
+		print person
+		print ""
 
-print data[3][0]
+webpro_database = []
+for person in people_person:
+	if 'Webprogrammeren en databases' in person:
+		webpro_database.append(person)
+
+print webpro_database
+print len(webpro_database)
 
 rooster.close()
