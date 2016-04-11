cf11212cb697bd56c5fd27f38999c91ac3e770f6
popoiopo <bastiaan.chatel@gmail.com>
2016-04-11 21:46:11 +0200

    helemaal fully functional

--- roostering.py
+++ roostering.py
@@ -17,21 +17,19 @@ var_names = [name for name in data[0]]
 people_var = [dict(zip(var_names, person)) for person in data]
 people_person = [dict(zip(person, var_names)) for person in data]
 
-for person in people_var:
-	print person['Voornaam']
-	print ""
+vakken = ["Advanced_Heuristics","Algoritmen_en_complexiteit","Analysemethoden_en_technieken","Architectuur_en_computerorganisatie","Autonomous_Agents_2","Bioinformatica","Calculus_2","Collectieve_Intelligentie","Compilerbouw","Compilerbouw_practicum","Data_Mining","Databases_2","Heuristieken_1","Heuristieken_2","Informatie_en_organsatieontwerp","Interactie_ontwerp","Kansrekenen_2","Lineaire_Algebra","Machine_Learning","Moderne_Databases","Netwerken_en_systeembeveiliging","Programmeren_in_Java_2","Project_Genetic_Algorithms","Project_Numerical_Recipes","Reflectie_op_de_digitale_cultuur","Software_engineering","Technology_for_games","Webprogrammeren_en_databases","Zoeken_sturen_en_bewegen"]
+vakken_database = {}
 
-for person in people_var:
-	if person['Vak1'] in 'Webprogrammeren en databases':
-		print person
-		print ""
+for vak in vakken:
+	for person in people_person:
+		if vak in person:
+			vakken_database.setdefault(vak, []).append(person)
 
-webpro_database = []
-for person in people_person:
-	if 'Webprogrammeren en databases' in person:
-		webpro_database.append(person)
+print "In totaal hebben wij " + str(len(people_person)) + " studenten, en " + str(len(vakken)) + " vakken."
+print "In de komende rijen zie je het vak en het aantal leerlingen die daarin deelnemen in het volgende format."
+print "Vaknaam : hoeveelheid leerlingen."
+for vak in vakken:
+	print vak + " : " + str(len(vakken_database[vak])) + "."
 
-print webpro_database
-print len(webpro_database)
 
 rooster.close()
